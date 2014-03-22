ff156aa6ca068e3b19eabbf328108fb30a572f15
Shi Zhenzhong <messi.shizz@gmail.com>
2014-03-22 23:23:25 +0800

    sutup init content script and design more icons

--- js/passKeeper.js
+++ js/passKeeper.js
@@ -1,1 +1,19 @@
+(function($) {
+    'use strict';
 
+    var downkeys = {
+        '17': false, // ctrl key
+        '73': false // character i
+    };
+
+    $(document).keydown(function(event) {
+        downkeys[event.which] = true;
+        if (downkeys['17'] && downkeys['73']) {
+            // TODO:bind ctrl-i to show passkeeper prompt window 
+            console.log('ctrl-i');
+        }
+
+    }).keyup(function(event) {
+        downkeys[event.which] = false;
+    });
+})(jQuery);
