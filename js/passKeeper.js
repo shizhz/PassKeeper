396ee3a74a9794d59608d09a229571a50037925e
Shi Zhenzhong <messi.shizz@gmail.com>
2014-03-23 18:34:50 +0800

    more code

--- js/passKeeper.js
+++ js/passKeeper.js
@@ -1,19 +1,22 @@
 (function($) {
     'use strict';
 
-    var downkeys = {
-        '17': false, // ctrl key
-        '73': false // character i
-    };
+    function bindHotKeys() {
+        var downkeys = {
+            '17': false, // ctrl key
+            '73': false // combination key: i
+        };
 
-    $(document).keydown(function(event) {
-        downkeys[event.which] = true;
-        if (downkeys['17'] && downkeys['73']) {
-            // TODO:bind ctrl-i to show passkeeper prompt window 
-            console.log('ctrl-i');
-        }
+        return function() {
+            $(document).keydown(function(event) {
+                downkeys[event.which] = true;
+                if (downkeys['17'] && downkeys['73']) {}
 
-    }).keyup(function(event) {
-        downkeys[event.which] = false;
-    });
+            }).keyup(function(event) {
+                downkeys[event.which] = false;
+            });
+        };
+    }
+
+
 })(jQuery);
