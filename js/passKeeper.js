9ff3de4eda25d4bc3fed1f1a42041832bbd7d416
shizhz <messi.shizz@gmail.com>
2014-03-24 18:42:59 +0800

    menu layout is done

--- js/passKeeper.js
+++ js/passKeeper.js
@@ -1,6 +1,31 @@
 (function($) {
     'use strict';
 
+    var MenuWatcher = {
+        menus: {
+            'menu-login': 'passkeeper-login',
+            'menu-query': 'passkeeper-query'
+        },
+
+        clicker: function() {
+            var menus = this.menus;
+            $.each(menus, function(key, value) {
+                $('#' + key).on('click', function(event) {
+                    event.preventDefault();
+                    $.each(menus, function(k, v) {
+                        $('#' + k).toggleClass('activeffect');
+                        $('#' + v).toggle();
+                    });
+                });
+            });
+        },
+
+        init: function() {
+            this.clicker();
+        },
+    };
+
+
     function PopupBox() {
         this.title_ = "Password for PassKeeper";
         this.error_msg_ = "Wrong password!";
@@ -8,25 +33,25 @@
     }
 
     PopupBox.prototype.build = function() {
-        var trigger = function() {
-            // TODO: trigger 
-        };
-        var pkBox = $('<div>').attr('id', 'passkeeper-box').addClass('passkeeper_popup_box');
-        $('<h1>').text(this.title_).appendTo(pkBox);
-        var passwd = $('<input>').attr('type', 'password').attr('name', 'password').attr('placeholder', 'Password').keypress(function(event) {
-            if (event.which == 13) {
-                trigger();
-            }
-        });
-        $('<p>').append(passwd).appendTo(pkBox);
-        var p = $('<p>').addClass('submit').appendTo(pkBox);
-        $('<input>').attr('type', 'button').attr('name', 'btn_go').attr('value', 'Go').on('click', trigger).appendTo(p);
-        $('<span>').addClass('passkeeper_error_msg').text(this.error_msg_).append($('<a href="#">').text(this.error_msg_reset_).on('click', function(event) {
-            // TODO: reset password
-        })).appendTo(p);
+        //var trigger = function() {
+        //// TODO: trigger 
+        //};
+        //var pkBox = $('<div>').attr('id', 'passkeeper-box').addClass('passkeeper_popup_box');
+        //$('<h1>').text(this.title_).appendTo(pkBox);
+        //var passwd = $('<input>').attr('type', 'password').attr('name', 'password').attr('placeholder', 'Password').keypress(function(event) {
+        //if (event.which == 13) {
+        //trigger();
+        //}
+        //});
+        //$('<p>').append(passwd).appendTo(pkBox);
+        //var p = $('<p>').addClass('submit').appendTo(pkBox);
+        //$('<input>').attr('type', 'button').attr('name', 'btn_go').attr('value', 'Go').on('click', trigger).appendTo(p);
+        //$('<span>').addClass('passkeeper_error_msg').text(this.error_msg_).append($('<a href="#">').text(this.error_msg_reset_).on('click', function(event) {
+        //// TODO: reset password
+        //})).appendTo(p);
 
-        return pkBox;
-    }
+        //return pkBox;
+    };
 
     function bindHotKeys() {
         var downkeys = {
@@ -45,5 +70,6 @@
         };
     }
 
+    MenuWatcher.init();
 
 })(jQuery);
